70db13c1ec93a2a044705057d852393f581b5be0
Ryusei-oss <ryusei945@gmail.com>
2020-06-26 05:03:26 +0900

    add incremanetal-search

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,32 +1,35 @@
-<<<<<<< Updated upstream
 $(function(){ 
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class=".message_list">
-        <div class=".message_list__contents--name">
-          ${message.user_name}
+      `<div class = "message" data-message-id = ${message.id}>
+        <div class=".message_list">
+          <div class=".message_list__contents--name">
+            ${message.user_name}
+          </div>
+          <div class="message_list__contents--date">
+            ${message.created_at}
+          </div>
+          <div class="message_list__contents--comment">
+            ${message.content}
+          </div>
+          <img src=${message.image} >
         </div>
-        <div class="message_list__contents--date">
-          ${message.created_at}
-        </div>
-        <div class="message_list__contents--comment">
-          ${message.content}
-        </div>
-        <img src=${message.image} >
       </div>`
      return html;
    } else {
      var html =
-      `<div class = "message_list__contents">
-        <div class="message_list__contents--name">
-          ${message.user_name}
-        </div>
-        <div class="message_list__contents--date">
-          ${message.created_at}
-        </div>
-        <div class="message_list__contents--comment">
-          ${message.content}
+      `<div class ="message" data-message-id = ${message.id}>
+        <div class = "message_list__contents">
+          <div class="message_list__contents--name">
+            ${message.user_name}
+          </div>
+          <div class="message_list__contents--date">
+            ${message.created_at}
+          </div>
+          <div class="message_list__contents--comment">
+            ${message.content}
+          </div>
         </div>
       </div>`
      return html;
@@ -54,12 +57,37 @@ $(function(){
       $('input').removeAttr("disabled");
     });
   })
+
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通りのURLを指定
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        var insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.chat_main__message_list').append(insertHTML);
+        $('.chat_main__message_list').animate({ scrollTop: $('.chat_main__message_list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
-=======
-$(function(){
-  $('#new_message').on('submit', function(e){
-    console.log('hoge');
-    e.preventDefault()
-  });
-});
->>>>>>> Stashed changes
